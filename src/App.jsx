e725ce3b2eb1c217f42cb9e8f396adaacf40499a
Fernando Casais <fpcasais@gmail.com>
2022-12-23 21:43:41 -0300

    formateo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-
-
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <>
-     
       <div className="App">
         <NavBar />
       </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,40 +1,39 @@
 import React from "react";
 import "./NavBar.css";
-import CartWidget from '../../components/CartWidget/CartWidget';
+import CartWidget from "../../components/CartWidget/CartWidget";
 import Header from "../../components/Header/Header";
-
 
 function NavBar(props) {
   return (
     <div>
-       <div>
+      <div>
         <Header />
         <div className="navSearch">
-        <nav className="navbar navbar-expand-lg .bg-secondary.bg-gradient">
-          <form class="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Qué estás buscando?"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </nav>
+          <nav className="navbar navbar-expand-lg .bg-secondary.bg-gradient">
+            <form class="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Qué estás buscando?"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </nav>
+        </div>
       </div>
-      </div>
-      
+
       <ul className="nav">
         <a href="#">Comedias</a>
         <a href="#">Románticas</a>
         <a href="#">Drama</a>
         <a href="#">Documentales</a>
         <a href="#">Series</a>
-        <a href="#">Suspenso</a><CartWidget/>
+        <a href="#">Suspenso</a>
+        <CartWidget />
       </ul>
-      
     </div>
   );
 }
